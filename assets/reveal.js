e8f5d70c79c47975f384aaf0e544ca3a1dabd84a
Joao Paulo S Leite <joaopaulofismed@gmail.com>
2022-12-18 20:59:18 -0300

    Implementando Scroll Animation com Lib-ScrollReveal

---
+++ assets/reveal.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal({reset: true});
+
+ScrollReveal().reveal(".professional-experience-container-title", {
+    duration: 1000
+});
+ScrollReveal().reveal(".profession-qualification-container-title",
+ {duration: 1000}
+ );
+
+ScrollReveal().reveal(".FMtitle", {
+    delay: 200,
+    duration: 1500,
+    origin: 'left',
+    distance: '200px'
+})
+ScrollReveal().reveal(".profile-container-img", {
+    delay: 200,
+    duration: 1500,
+    origin: 'Right',
+    distance: '200px'
+})
+ScrollReveal().reveal(".profession-qualification-card", {
+    delay: 200,
+    duration: 1200,
+    origin: 'left',
+    distance: '200px'
+});
+ScrollReveal().reveal(".professional-experience-card", {
+    delay: 200,
+    duration: 1200,
+    origin: 'bottom',
+    distance: '200px'
+});
