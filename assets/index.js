433c2db8c2f5521f89ebef489806e75d650a7beb
Joao Paulo S Leite <joaopaulofismed@gmail.com>
2022-12-21 19:51:36 -0300

    Adicionando Servico de Email

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 const hamburguer = document.querySelector(".hamburguer")
 const navMenu = document.querySelector(".nav-menu")
+const btnSendEmail = document.getElementById("btn-send")
 
 hamburguer.addEventListener("click", () =>{
     hamburguer.classList.toggle("active")
@@ -11,3 +12,17 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active")
 }))
 
+btnSendEmail.addEventListener("click", (evt) =>{
+    evt.preventDefault()
+    var params = {
+        from_name : document.getElementById("name").value,
+        email_id : document.getElementById("email").value,
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_pv4e57e", "template_sre4ym8", params).then(function (){
+        alert("Obrigado Pelo contato!!! E-mail enviado com Sucesso!!!")
+        document.getElementById("name").value = ""
+        document.getElementById("email").value = ""
+        document.getElementById("message").value = ""
+    })
+})
