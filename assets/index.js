ac1a886ff16b287d718ec48734042a073558166e
Joao Paulo S Leite <joaopaulofismed@gmail.com>
2023-01-04 20:02:00 -0300

    Implementando a validação do Formulario de E-mail

--- assets/index.js
+++ assets/index.js
@@ -1,28 +1,76 @@
-const hamburguer = document.querySelector(".hamburguer")
-const navMenu = document.querySelector(".nav-menu")
-const btnSendEmail = document.getElementById("btn-send")
+const hamburguer = document.querySelector('.hamburguer');
+const navMenu = document.querySelector('.nav-menu');
 
-hamburguer.addEventListener("click", () =>{
-    hamburguer.classList.toggle("active")
-    navMenu.classList.toggle("active")
-})
+const tooltipEmail = document.querySelector('.tooltipEmail');
+const inputs = document.querySelectorAll('.input-item');
+const btnSendEmail = document.getElementById('btn-send');
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburguer.classList.remove("active")
-    navMenu.classList.remove("active")
-}))
+hamburguer.addEventListener('click', () => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
 
-btnSendEmail.addEventListener("click", (evt) =>{
-    evt.preventDefault()
-    var params = {
-        from_name : document.getElementById("name").value,
-        email_id : document.getElementById("email").value,
-        message : document.getElementById("message").value
-    }
-    emailjs.send("service_pv4e57e", "template_sre4ym8", params).then(function (){
-        alert("Obrigado Pelo contato!!! E-mail enviado com Sucesso!!!")
-        document.getElementById("name").value = ""
-        document.getElementById("email").value = ""
-        document.getElementById("message").value = ""
+document.querySelectorAll('.nav-link').forEach((n) =>
+    n.addEventListener('click', () => {
+        hamburguer.classList.remove('active');
+        navMenu.classList.remove('active');
     })
-})
+);
+
+function inputValidation(inp) {
+    if (inp.value === '') {
+        inp.style.borderColor = '#ff0000';
+        tooltipEmail.classList.remove('active');
+        inp.focus();
+        return false;
+    } else if (inp.type === 'email') {
+        const regexEmail = new RegExp(
+            /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum|br|io)\b/
+        );
+        if (!regexEmail.test(inp.value)) {
+            inp.style.borderColor = '#ff0000';
+            inp.focus();
+            tooltipEmail.classList.add('active');
+            return false;
+        } else {
+            inp.style.borderColor = '#384459';
+            tooltipEmail.classList.remove('active');
+            return true;
+        }
+    } else {
+        inp.style.borderColor = '#384459';
+        return true;
+    }
+}
+inputs.forEach((inp) =>
+    inp.addEventListener('input', () => {
+        inputValidation(inp);
+    })
+);
+
+btnSendEmail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    let error = false;
+    inputs.forEach(function (inp) {
+        if (!inputValidation(inp)) {
+            error = true;
+        }
+    });
+    if (error) {
+        alert('Todos os campos devem ser preenchidos corretamente!!!');
+    } else {
+        var params = {
+            from_name: document.getElementById('name').value,
+            email_id: document.getElementById('email').value,
+            message: document.getElementById('message').value,
+        };
+        emailjs
+            .send('service_pv4e57e', 'template_sre4ym8', params)
+            .then(function () {
+                alert('Obrigado Pelo contato!!! E-mail enviado com Sucesso!!!');
+                document.getElementById('name').value = '';
+                document.getElementById('email').value = '';
+                document.getElementById('message').value = '';
+            });
+    }
+});
--- assets/reveal.js
+++ assets/reveal.js
@@ -1,62 +1,61 @@
-window.sr = ScrollReveal({reset: true});
+window.sr = ScrollReveal({ reset: true });
 
-ScrollReveal().reveal(".professional-experience-container-title", {
-    duration: 1000
+ScrollReveal().reveal('.professional-experience-container-title', {
+    duration: 1000,
 });
-ScrollReveal().reveal(".profession-qualification-container-title",
- {duration: 1000}
- );
+ScrollReveal().reveal('.profession-qualification-container-title', {
+    duration: 1000,
+});
 
-ScrollReveal().reveal(".FMtitle", {
+ScrollReveal().reveal('.FMtitle', {
     delay: 200,
     duration: 1500,
     origin: 'left',
-    distance: '200px'
-})
-ScrollReveal().reveal(".profile-container-img", {
+    distance: '200px',
+});
+ScrollReveal().reveal('.profile-container-img', {
     delay: 200,
     duration: 1500,
     origin: 'Right',
-    distance: '200px'
-})
-ScrollReveal().reveal(".profession-qualification-card", {
+    distance: '200px',
+});
+ScrollReveal().reveal('.profession-qualification-card', {
     delay: 200,
     duration: 1200,
     origin: 'Right',
-    distance: '200px'
+    distance: '200px',
 });
-ScrollReveal().reveal(".professional-experience-card", {
+ScrollReveal().reveal('.professional-experience-card', {
     delay: 200,
     duration: 1200,
     origin: 'left',
-    distance: '200px'
+    distance: '200px',
 });
 
-ScrollReveal().reveal(".skills-container-title img", {
+ScrollReveal().reveal('.skills-container-title img', {
     delay: 200,
     duration: 1500,
-    distance: '200px'
-})
-ScrollReveal().reveal(".skills-container-title h3", {
+    distance: '200px',
+});
+ScrollReveal().reveal('.skills-container-title h3', {
     delay: 200,
     duration: 1500,
     origin: 'Left',
-    distance: '200px'
-})
-ScrollReveal().reveal(".skills-list-item", {
+    distance: '200px',
+});
+ScrollReveal().reveal('.skills-list-item', {
     delay: 200,
     duration: 1200,
     origin: 'bottom',
-    distance: '200px'
+    distance: '200px',
 });
-ScrollReveal().reveal(".footer-container-title", {
+ScrollReveal().reveal('.footer-container-title', {
     delay: 200,
     duration: 1500,
     origin: 'left',
-    distance: '200px'
-})
-ScrollReveal().reveal("#footer-form",
- {  
+    distance: '200px',
+});
+ScrollReveal().reveal('#footer-form', {
     delay: 200,
     duration: 1500,
 });
